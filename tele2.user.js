3bbc3aa17acb925f9d86fcad09bd6c82e807c012
rastasimonbonface <rastasimonbonface@gmail.com>
2024-06-19 13:28:46 +0200

    Create tele2.user.js

---
+++ tele2.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Tele2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Rastasimon
+// @match        https://web.telegram.org/k/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    document.addEventListener("keydown", (event) => {if (event.keyCode === 17) {
+        document.querySelector('div.media-viewer-buttons > button:nth-child(3)').classList.remove("hide")
+        document.querySelector('div.media-viewer-buttons > button:nth-child(3)').click()
+    }})
+})();
